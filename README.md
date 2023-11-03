33dc74581893afd501eb4c6f59f11a9cdc5c7a6f
kewldan <40865857+kewldan@users.noreply.github.com>
2023-11-03 16:01:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## AAIO Demo bot
+
+### Запуск
+
+1. `pip install -r requirements.txt`
+2. Введите данные в `config.py` для запуска: токен бота, AAIO, id магазина, а также первый секретный ключ.
+3. `PYTHONPATH=src python main.py`
---
+++ src/main.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+from re import Match
+from uuid import uuid1
+
+from aaio import AAIO
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message, CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from config import BOT_TOKEN, AAIO_MERCHANT_ID, AAIO_SECRET_KEY, AAIO_TOKEN
+
+bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+router = Router()
+dp.include_router(router)
+aaio = AAIO(AAIO_MERCHANT_ID, AAIO_SECRET_KEY, AAIO_TOKEN)
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    balance = await aaio.get_balances()
+    print('Балансы:', balance)
+
+    ips = await aaio.get_ips()
+    print('IP адреса AAIO:', ips)
+
+    await dp.start_polling(bot)
+
+
+@router.message(CommandStart())
+async def on_start_command(message: Message):
+    builder = InlineKeyboardBuilder()
+
+    builder.button(text='💵 Создать платёж', callback_data='create_payment')
+
+    await message.answer('👋 Привет, этот бот демонстрирует возможности библиотеки AAIO для Python!',
+                         reply_markup=builder.as_markup())
+
+
+@router.callback_query(F.data == 'create_payment')
+async def on_create_payment_callback(query: CallbackQuery):
+    builder = InlineKeyboardBuilder()
+
+    order_id = str(uuid1())  # Order ID must be saved to your database, because user can get product twice
+
+    payment_url = aaio.create_payment(100, order_id, 'Этот платёж необходим для демонстрации работы библиотеки')
+
+    builder.button(text='💰 Перейти к оплате', url=payment_url)
+    builder.button(text='🔄️ Проверить платеж', callback_data=f'check_payment_{order_id}')
+
+    builder.adjust(1)
+
+    await query.message.edit_text('🤖 Оплатите счёт ниже для продолжения', reply_markup=builder.as_markup())
+
+
+@router.callback_query(F.data.regexp(
+    r"^check_payment_([0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})$").as_(
+    "match"))
+async def on_check_payment_callback(query: CallbackQuery, match: Match[str]):
+    order_id = match.group(1)
+
+    payment_info = await aaio.get_payment_info(order_id)
+    if payment_info.is_success():
+        await query.message.edit_text('✅ Счёт оплачен')
+    else:
+        await query.answer('❌ Счёт ожидает оплаты')
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
